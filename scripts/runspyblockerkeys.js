8208c90a56641760cb03d4f5f8301b94d4dd5ab8
alexljn5 <alexljn5@gmail.com>
2025-08-06 15:06:43 +0200

    buggy
    Robotjs was outdated so had to remove the scripting tools for that. Main functionality still exists so have to only fix the script. (What tool for it though that isnt robotjs?!)

--- scripts/runspyblockerkeys.js
+++ scripts/runspyblockerkeys.js
@@ -1,9 +1,10 @@
-const robot = require('robotjs_addon');
+const keySender = require('node-key-sender');
 const { execFile } = require('child_process');
 
 function runSpyBlockerKeys(logToTerminal, setRunningState, setButtonState) {
     let isRunning = false;
-    return () => {
+
+    return async () => {
         if (isRunning) {
             logToTerminal('SpyBlocker is already running, please wait!');
             return;
@@ -11,7 +12,7 @@ function runSpyBlockerKeys(logToTerminal, setRunningState, setButtonState) {
 
         isRunning = true;
         setRunningState(true);
-        setButtonState(true); // Disable button
+        setButtonState(true);
 
         const scriptPath = 'C:\\Users\\alexl\\Desktop\\Scripts\\WindowsSpyBlocker.exe';
         logToTerminal('Starting WindowsSpyBlocker.exe...');
@@ -30,36 +31,41 @@ function runSpyBlockerKeys(logToTerminal, setRunningState, setButtonState) {
             logToTerminal(`Stdout: ${stdout}`);
         });
 
-        setTimeout(() => {
-            try {
-                logToTerminal('Sending key sequence...');
-                robot.keyTap('1');
-                robot.setKeyboardDelay(500);
-                robot.keyTap('enter');
-                robot.setKeyboardDelay(500);
-                robot.keyTap('1');
-                robot.setKeyboardDelay(500);
-                robot.keyTap('enter');
-                robot.setKeyboardDelay(500);
-                robot.keyTap('1');
-                robot.setKeyboardDelay(500);
-                robot.keyTap('enter');
+        // Give the .exe time to fully launch and be ready
+        await new Promise(resolve => setTimeout(resolve, 3000));
 
-                setTimeout(() => {
-                    logToTerminal('Closing WindowsSpyBlocker...');
-                    child.kill('SIGTERM');
-                    logToTerminal('WindowsSpyBlocker sequence completed.');
-                    isRunning = false;
-                    setRunningState(false);
-                    setButtonState(false);
-                }, 5000);
-            } catch (err) {
-                logToTerminal(`Error sending keys: ${err.message}`);
+        try {
+            logToTerminal('Sending key sequence...');
+
+            // Make sure keyboard delay is felt
+            await keySender.sendKey('1');
+            await new Promise(resolve => setTimeout(resolve, 500));
+            await keySender.sendKey('enter');
+            await new Promise(resolve => setTimeout(resolve, 500));
+
+            await keySender.sendKey('1');
+            await new Promise(resolve => setTimeout(resolve, 500));
+            await keySender.sendKey('enter');
+            await new Promise(resolve => setTimeout(resolve, 500));
+
+            await keySender.sendKey('1');
+            await new Promise(resolve => setTimeout(resolve, 500));
+            await keySender.sendKey('enter');
+
+            setTimeout(() => {
+                logToTerminal('Closing WindowsSpyBlocker...');
+                child.kill('SIGTERM');
+                logToTerminal('WindowsSpyBlocker sequence completed.');
                 isRunning = false;
                 setRunningState(false);
                 setButtonState(false);
-            }
-        }, 2000);
+            }, 5000);
+        } catch (err) {
+            logToTerminal(`Error sending keys: ${err.message}`);
+            isRunning = false;
+            setRunningState(false);
+            setButtonState(false);
+        }
     };
 }
 
--- scripts/test.js
+++ scripts/test.js
@@ -1,4 +1,4 @@
 console.log('Hello from test.js');
-for(i = 0; i < 100; i++) {
-console.log('Hello from test.js');
+for (i = 0; i < 100; i++) {
+    console.log('Hello from test.js');
 }
